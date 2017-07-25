019a25052d65a34484289a42c8536043662e5f04
Ana <ana.toma92@gmail.com>
2017-07-25 23:02:10 +0300

    add day 3

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 This repository tracks the #dailycssimages challenge. More info about the challenge is available [here.](http://dailycssimages.com/)
 
-###Week 1
+### Display
+
+0. [#cssdailyimages Portfolio](https://codepen.io/feuerbird29/full/gxObKv/)
+
+### Week 1
 
 1. [Bear Cub](https://codepen.io/feuerbird29/pen/JWOBdV)
 2. [Elephant](https://codepen.io/feuerbird29/full/jLNjmw/)
-3. Beaver
+3. [Beaver](https://codepen.io/feuerbird29/full/OjPRmm/)
 4. Tiger
 5. Dory from Finding Nemo
 
---
+++ day-0-portfolio/js/index.js
@@ -0,0 +1,210 @@
+"use strict";
+
+var Header = React.createClass({
+  displayName: "Header",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "container-fluid" },
+      React.createElement(
+        "div",
+        { className: "row col-md-12" },
+        React.createElement(
+          "h1",
+          null,
+          "Ana Toma"
+        ),
+        React.createElement(
+          "a",
+          { href: "https://anawebdev.surge.sh", target: "_blank" },
+          React.createElement(
+            "h3",
+            null,
+            "- portfolio -"
+          )
+        ),
+        React.createElement(
+          "a",
+          { href: "http://dailycssimages.com", target: "_blank" },
+          React.createElement(
+            "h3",
+            null,
+            "#dailycsssimages"
+          )
+        )
+      )
+    );
+  }
+});
+
+var Display = React.createClass({
+  displayName: "Display",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "col-md-2" },
+      React.createElement(
+        "a",
+        { href: this.props.url, target: "_blank" },
+        React.createElement("img", { className: "img-fluid center-block", src: this.props.img })
+      )
+    );
+  }
+});
+
+var Title = React.createClass({
+  displayName: "Title",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "row" },
+      React.createElement(
+        "div",
+        { className: "col-md-12" },
+        React.createElement(
+          "h2",
+          null,
+          "Week ",
+          this.props.week
+        ),
+        React.createElement(
+          "h4",
+          null,
+          this.props.text
+        )
+      )
+    );
+  }
+});
+
+var Section = React.createClass({
+  displayName: "Section",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "container-fluid group" },
+      React.createElement(Title, { week: this.props.week, text: this.props.text }),
+      React.createElement(
+        "div",
+        { className: "row" },
+        React.createElement("div", { className: "col-md-1" }),
+        React.createElement(Display, { img: this.props.img1, url: this.props.url1 }),
+        React.createElement(Display, { img: this.props.img2, url: this.props.url2 }),
+        React.createElement(Display, { img: this.props.img3, url: this.props.url3 }),
+        React.createElement(Display, { img: this.props.img4, url: this.props.url4 }),
+        React.createElement(Display, { img: this.props.img5, url: this.props.url5 })
+      )
+    );
+  }
+});
+
+var App = React.createClass({
+  displayName: "App",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(Header, null),
+      React.createElement(Section, {
+        week: "1",
+        text: "- cute animals -",
+        img1: "http://i.imgur.com/5ZooWEN.jpg",
+        img2: "http://i.imgur.com/w4zaL04.jpg",
+        img3: "http://i.imgur.com/dCVqWpP.jpg",
+        img4: "http://via.placeholder.com/150x150",
+        img5: "http://via.placeholder.com/150x150",
+        url1: "https://codepen.io/feuerbird29/full/JWOBdV/",
+        url2: "https://codepen.io/feuerbird29/full/jLNjmw/",
+        url3: "https://codepen.io/feuerbird29/full/OjPRmm/"
+      }),
+      React.createElement(Section, {
+        week: "2",
+        text: "- office objects -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "3",
+        text: "- yummy food -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "4",
+        text: "- monsters and zombies -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "5",
+        text: "- superheroes -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "6",
+        text: "- icons -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "7",
+        text: "- logos -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "8",
+        text: "- electronics -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "9",
+        text: "- monsters and zombies -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      }),
+      React.createElement(Section, {
+        week: "10",
+        text: "- celebrations and parties -",
+        img1: "",
+        img2: "",
+        img3: "",
+        img4: "",
+        img5: ""
+      })
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
